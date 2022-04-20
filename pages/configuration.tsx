60dec5f488eae0b1799133001ac9a162a92f1499
Zaiste <oh@zaiste.net>
2022-04-20 11:56:34 +0200

    add an example of a GraphQL query

--- pages/configuration.tsx
+++ pages/configuration.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useFetchTwelveProductsQuery } from '../generated/graphql'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+  const [{ data, loading, error }] = useFetchTwelveProductsQuery()
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,21 +16,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        Saleor App Template - Configuration
+        <div className="text-white">Saleor App Template - Configuration</div>
+        {JSON.stringify(data)}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
