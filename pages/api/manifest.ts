1b1434ae1ef030c908ca92aa93bf55cd4359cf14
Zaiste <oh@zaiste.net>
2022-04-23 22:22:44 +0200

    reload the `APP_URL` env variable

--- pages/api/manifest.ts
+++ pages/api/manifest.ts
@@ -1,14 +1,20 @@
 import { NextApiHandler } from "next"
 import { version, name } from "../../package.json"
+import dotenv from 'dotenv'
+import fs from 'fs/promises';
 
-const handler: NextApiHandler = (req, res) => {
+const handler: NextApiHandler = async (req, res) => {
+  const content = await fs.readFile('.env', 'utf-8');
+  const { APP_URL } = dotenv.parse(content);
+  process.env.APP_URL = APP_URL 
+
   const manifest = {
     id: "saleor.app",
     version: version,
     name: name,
     permissions: ['MANAGE_ORDERS'],
-    configurationUrl: `${process.env.NEXT_PUBLIC_APP_URL}/configuration`,
-    tokenTargetUrl: `${process.env.NEXT_PUBLIC_APP_URL}/api/register`,
+    configurationUrl: `${process.env.APP_URL}/configuration`,
+    tokenTargetUrl: `${process.env.APP_URL}/api/register`,
   }
 
   res.end(JSON.stringify(manifest))
