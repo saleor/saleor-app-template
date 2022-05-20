379e237b71e0d7fb137c479c820b53341839deb1
Tomasz Magulski <tomasz.magulski@saleor.io>
2022-05-20 15:09:03 +0200

    [SALEOR-6519] Configuration form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,28 @@
+import React, { useEffect, PropsWithChildren } from "react";
 import type { AppProps } from "next/app";
+import { ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
 
 import "../styles/globals.css";
 import AppBridgeProvider from "../providers/AppBridgeProvider";
 import GraphQLProvider from "../providers/GraphQLProvider";
 
+// That's a hack required by Macaw-UI incompitability with React@18
+const ThemeProvider = MacawUIThemeProvider as React.FC<PropsWithChildren<{}>>;
+
 const SaleorApp = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <AppBridgeProvider>
       <GraphQLProvider>
-        <Component {...pageProps} />
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </GraphQLProvider>
     </AppBridgeProvider>
   );
--- pages/configuration.tsx
+++ pages/configuration.tsx
@@ -1,12 +1,30 @@
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent, SyntheticEvent } from "react";
+import { Card, CardHeader, CardContent, TextField } from "@material-ui/core";
+import { ConfirmButton, ConfirmButtonTransitionState, makeStyles } from "@saleor/macaw-ui";
 
 import { SALEOR_DOMAIN_HEADER } from "../constants";
 import useApp from "../hooks/useApp";
 
+interface ConfigurationField {
+  key: string;
+  value: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  confirmButton: {
+    marginLeft: "auto",
+  },
+  fieldContainer: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 const Configuration: NextPage = () => {
+  const classes = useStyles();
   const appState = useApp()?.getState();
-  const [configuration, setConfiguration] = useState(null);
+  const [configuration, setConfiguration] = useState<ConfigurationField[]>();
+  const [transitionState, setTransitionState] = useState<ConfirmButtonTransitionState>("default");
 
   useEffect(() => {
     appState?.domain && appState?.token && fetch(
@@ -20,18 +38,72 @@ const Configuration: NextPage = () => {
         .then(({ data }) => setConfiguration(data))
   }, [appState]);
 
-  if (!appState?.ready) {
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    setTransitionState("loading");
+
+    fetch(
+      "/api/configuration",
+      {
+        method: "POST",
+        headers: [
+          ["content-type", "application/json"],
+          [SALEOR_DOMAIN_HEADER, appState?.domain!],
+          ["authorization-bearer", appState?.token!],
+        ],
+        body: JSON.stringify({ data: configuration }),
+      },
+    ).then(
+      (response) => setTransitionState(response.status === 200 ? "success": "error")
+    ).catch(
+      () => setTransitionState("error")
+    );
+  };;
+
+  const onChange = (event: ChangeEvent) => {
+    const { name, value } = event.target as HTMLInputElement;
+    setConfiguration((prev) =>
+      prev!.map((prevField) =>
+        prevField.key === name ? { ...prevField, value } : prevField
+      )
+    );
+  };
+
+  if (!appState?.ready || configuration === undefined) {
     return <div className="text-white">Loading...</div>;
   }
 
   return (
-    <div>
-      <main>
-        <div className="text-white">Saleor App Template - Configuration</div>
-        <div className="text-white">{appState?.domain}</div>
-        <div className="text-white">{JSON.stringify(configuration)}</div>
-      </main>
-    </div>
+    <Card>
+      <CardHeader title="Configuration" />
+      <CardContent>
+        <form onSubmit={handleSubmit}>
+          {configuration!.map(({ key, value }) => (
+            <div key={key} className={classes.fieldContainer}>
+              <TextField
+                label={key}
+                name={key}
+                fullWidth
+                onChange={onChange}
+                value={value}
+              />
+            </div>
+          ))}
+          <div>
+            <ConfirmButton
+              type="submit"
+              variant="primary"
+              transitionState={transitionState}
+              labels={{
+                confirm: "Save",
+                error: "Error",
+              }}
+              className={classes.confirmButton}
+            />
+          </div>
+        </form>
+      </CardContent>
+    </Card>
   );
 };
 
