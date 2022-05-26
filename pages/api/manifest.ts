731835906c4fdb21760a5d8f525339a20ec5c37f
Tomasz Magulski <tomasz.magulski@saleor.io>
2022-05-26 13:47:42 +0200

    Improved webhooks inference
     * forcing subscription to exist
     * infering event type based on types generated from GraphQL schema

--- pages/api/manifest.ts
+++ pages/api/manifest.ts
@@ -1,27 +1,52 @@
 import { NextApiHandler } from "next";
 import { print } from "graphql/language/printer";
-import fg from 'fast-glob';
-import path from 'path';
+import fg from "fast-glob";
+import path from "path";
 
 import { version, name } from "../../package.json";
 import * as GeneratedGraphQL from "../../generated/graphql";
 import { getBaseURL } from "../../lib/middlewares";
 
-const capitalize = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
+const capitalize = (value: string) =>
+  value.charAt(0).toUpperCase() + value.slice(1);
 const dropFileExtension = (filename: string) => path.parse(filename).name;
 
 const inferWebhooks = async (baseURL: string) => {
-  const entries = await fg(['*.ts'], { cwd: 'pages/api/webhooks' });
+  const entries = await fg(["*.ts"], { cwd: "pages/api/webhooks" });
 
   return entries.map(dropFileExtension).map((name: string) => {
-    const camelcaseName = name.split('-').map(capitalize).join('');
+    const camelcaseName = name.split("-").map(capitalize).join("");
+
+    const eventName = name.toUpperCase().replace(new RegExp("-", "g"), "_");
+    let eventType: string;
+    console.log();
+    switch (true) {
+      case Object.values(GeneratedGraphQL.WebhookEventTypeAsyncEnum).includes(
+        eventName as GeneratedGraphQL.WebhookEventTypeAsyncEnum
+      ):
+        eventType = "asyncEvents";
+        break;
+      case Object.values(GeneratedGraphQL.WebhookEventTypeSyncEnum).includes(
+        eventName as GeneratedGraphQL.WebhookEventTypeSyncEnum
+      ):
+        eventType = "syncEvents";
+        break;
+      default:
+        throw Error("Event type not found.");
+    }
+
     const statement = `${camelcaseName}SubscriptionDocument`;
-    const query = statement in  GeneratedGraphQL ? print((GeneratedGraphQL as any)[statement]) : null;
+    let query: string;
+    if (statement in GeneratedGraphQL) {
+      query = print((GeneratedGraphQL as any)[statement]);
+    } else {
+      throw Error("Subscription not found.");
+    }
 
     return {
       name,
-      asyncEvents: [name.toUpperCase().replace("-", "_")],
-      query, 
+      [eventType]: [eventName],
+      query,
       targetUrl: `${baseURL}/api/webhooks/${name}`,
     };
   });
@@ -53,6 +78,6 @@ const handler: NextApiHandler = async (request, response) => {
   };
 
   response.json(manifest);
-}
+};
 
 export default handler;
