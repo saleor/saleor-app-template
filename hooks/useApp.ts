f6b1a6a550594b2ad9fef9a78391984aa0d66701
Timur <timuric@gmail.com>
2022-07-01 11:48:58 +0200

    Subscribe to events and re-render

--- hooks/useApp.ts
+++ hooks/useApp.ts
@@ -1,9 +1,22 @@
-import { useContext } from "react";
+import {useContext, useEffect, useState} from "react";
 
 import { AppContext } from "../providers/AppBridgeProvider";
 
 const useApp = () => {
   const { app } = useContext(AppContext);
+  const [_, setState] = useState(0);
+
+  const update = ()=>{
+    setState(Math.random())
+  }
+
+  useEffect(()=>{
+    app?.subscribe("handshake",update);
+    app?.subscribe("theme",update);
+    app?.subscribe("response",update);
+    app?.subscribe("response",update);
+  },[])
+
   return app;
 };
 
