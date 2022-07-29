7100df21c7503e503248927294001dd5e47832c9
Wojciech <wojciech.mista@hotmail.com>
2022-07-29 10:43:39 +0200

    Send notifications via app-bridge

---
+++ utils/useDashboardNotifier.ts
@@ -0,0 +1,12 @@
+import { actions, NotificationPayload } from "@saleor/app-bridge";
+import useApp from "../hooks/useApp";
+
+const useDashboardNotifier = () => {
+  const app = useApp();
+  const notify = (payload: NotificationPayload) =>
+    app?.getState()?.ready && app?.dispatch(actions.Notification(payload));
+
+  return [notify];
+};
+
+export default useDashboardNotifier;
