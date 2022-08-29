448ae53fca58f2137fba3f0be1f65c5833bbe7a0
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-08-29 09:20:22 +0200

    Remove useMemo in AppBridgeProvider
    In React 18, useMemo is called twice in Strict mode in dev env.
    This causes bugs, because code inside is called twice, causing e.g.
    multiple listeners to be attached
    
    In app-template this memo is not required, because app should be global and
    can be passed directly into context

--- providers/AppBridgeProvider.tsx
+++ providers/AppBridgeProvider.tsx
@@ -1,5 +1,5 @@
 import { App, createApp } from "@saleor/app-bridge";
-import { createContext, PropsWithChildren, useMemo } from "react";
+import { createContext, PropsWithChildren } from "react";
 
 interface IAppContext {
   app?: App;
@@ -7,15 +7,16 @@ interface IAppContext {
 
 export const AppContext = createContext<IAppContext>({ app: undefined });
 
-function AppBridgeProvider(props: PropsWithChildren<{}>) {
-  const value = useMemo(() => {
-    if (typeof window !== "undefined") {
-      return { app: createApp() };
-    }
-    return { app: undefined };
-  }, []);
+let saleorAppBridgeInstance: IAppContext = { app: undefined };
 
-  return <AppContext.Provider value={value} {...props} />;
+if (typeof window !== "undefined") {
+  saleorAppBridgeInstance = {
+    app: createApp(),
+  };
+}
+
+function AppBridgeProvider(props: PropsWithChildren<{}>) {
+  return <AppContext.Provider value={saleorAppBridgeInstance} {...props} />;
 }
 
 export default AppBridgeProvider;
