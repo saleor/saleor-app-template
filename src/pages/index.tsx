446b1efe659b720b0d6da0e0326f4ae2ed1e7850
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-10-14 17:26:15 +0200

    Add dashboard actions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import { NextPage } from "next";
+import { useAppBridge } from "@saleor/app-sdk/app-bridge";
+import { Button } from "@saleor/macaw-ui";
+import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+
+const ClientContent = dynamic(() => import("../DashboardActions"), {
+  ssr: false,
+});
 
 /**
  * This is page publicly accessible from your app.
  * You should probably remove it.
  */
 const IndexPage: NextPage = () => {
+  const { appBridgeState } = useAppBridge();
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {setMounted(true)},[])
+
   return (
     <div>
       <h1>Welcome to Saleor App Template (Next.js) 🚀</h1>
@@ -39,6 +52,12 @@ const IndexPage: NextPage = () => {
           </a>
         </li>
       </ul>
+
+      {appBridgeState?.ready && mounted ? (
+        <ClientContent />
+      ) : (
+        <p>Install this app in your Dashboard and check extra powers!</p>
+      )}
     </div>
   );
 };
