124960f51236554a77f509b1805a82c368956194
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2022-10-26 14:33:49 +0200

    Fix links unclickable from iframe (#109)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import { useAppBridge } from "@saleor/app-sdk/app-bridge";
 import { Button } from "@saleor/macaw-ui";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 
 const ClientContent = dynamic(() => import("../DashboardActions"), {
   ssr: false,
@@ -13,22 +13,48 @@ const ClientContent = dynamic(() => import("../DashboardActions"), {
  * You should probably remove it.
  */
 const IndexPage: NextPage = () => {
-  const { appBridgeState } = useAppBridge();
-  const [mounted, setMounted] = useState(false)
+  const { appBridgeState, appBridge } = useAppBridge();
+  const [mounted, setMounted] = useState(false);
 
-  useEffect(() => {setMounted(true)},[])
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const handleLinkClick: MouseEventHandler<HTMLAnchorElement> = (e) => {
+    /**
+     * In iframe, link can't be opened in new tab, so Dashboard must be a proxy
+     */
+    if (appBridgeState?.ready) {
+      e.preventDefault();
+
+      appBridge?.dispatch({
+        type: "redirect",
+        payload: {
+          newContext: true,
+          actionId: "redirect-to-external-resource",
+          to: e.currentTarget.href,
+        },
+      });
+    }
+
+    /**
+     * Otherwise, assume app is accessed outside of Dashboard, so href attribute on <a> will work
+     */
+  };
 
   return (
     <div>
       <h1>Welcome to Saleor App Template (Next.js) 🚀</h1>
-      <p>
-        This is a boilerplate you can start with, to create an app
-        connected to Saleor
-      </p>
+      <p>This is a boilerplate you can start with, to create an app connected to Saleor</p>
       <h2>Resources</h2>
       <ul>
         <li>
-          <a target="_blank" rel="noreferrer" href="https://github.com/saleor/app-examples">
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/saleor/app-examples"
+          >
             App Examples repository
           </a>
         </li>
@@ -39,6 +65,7 @@ const IndexPage: NextPage = () => {
         </li>
         <li>
           <a
+            onClick={handleLinkClick}
             target="_blank"
             href="https://docs.saleor.io/docs/3.x/developer/extending/apps/key-concepts"
             rel="noreferrer"
@@ -47,7 +74,12 @@ const IndexPage: NextPage = () => {
           </a>
         </li>
         <li>
-          <a target="_blank" href="https://github.com/saleor/saleor-cli" rel="noreferrer">
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            href="https://github.com/saleor/saleor-cli"
+            rel="noreferrer"
+          >
             Saleor CLI
           </a>
         </li>
