886eb5503257398668cc6c63b062da1da8bd4102
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-02-07 14:44:18 +0100

    Remove mentions of VercelAPL

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -1,5 +1,5 @@
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
-import { APL, FileAPL, UpstashAPL, VercelAPL } from "@saleor/app-sdk/APL";
+import { APL, FileAPL, UpstashAPL } from "@saleor/app-sdk/APL";
 
 /**
  * By default auth data are stored in the `.auth-data.json` (FileAPL).
@@ -8,12 +8,8 @@ import { APL, FileAPL, UpstashAPL, VercelAPL } from "@saleor/app-sdk/APL";
  * To read more about storing auth data, read the
  * [APL documentation](https://github.com/saleor/saleor-app-sdk/blob/main/docs/apl.md)
  */
-
 export let apl: APL;
 switch (process.env.APL) {
-  case "vercel":
-    apl = new VercelAPL();
-    break;
   case "upstash":
     // Require `UPSTASH_URL` and `UPSTASH_TOKEN` environment variables
     apl = new UpstashAPL();
