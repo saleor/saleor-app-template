d142044e47bacd2b369e1f756390a9e484b01f18
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-09 10:49:38 +0200

    Update template to use new SDK and Macaw. Add example webhook

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,21 @@
+import "@saleor/macaw-ui/next/style";
 import "../styles/globals.css";
 
-import { Theme } from "@material-ui/core/styles";
 import { AppBridge, AppBridgeProvider } from "@saleor/app-sdk/app-bridge";
 import { RoutePropagator } from "@saleor/app-sdk/app-bridge/next";
-import { ThemeProvider as MacawUIThemeProvider } from "@saleor/macaw-ui";
-import React, { PropsWithChildren, useEffect } from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 
 import { ThemeSynchronizer } from "../lib/theme-synchronizer";
 import { NoSSRWrapper } from "../lib/no-ssr-wrapper";
 import { GraphQLProvider } from "../providers/GraphQLProvider";
-
-const themeOverrides: Partial<Theme> = {
-  /**
-   * You can override MacawUI theme here
-   */
-};
+import { ThemeProvider } from "@saleor/macaw-ui/next";
 
 /**
  * Ensure instance is a singleton.
  * TODO: This is React 18 issue, consider hiding this workaround inside app-sdk
  */
 const appBridgeInstance = typeof window !== "undefined" ? new AppBridge() : undefined;
-
-/**
- * That's a hack required by Macaw-UI incompatibility with React@18
- */
-const ThemeProvider = MacawUIThemeProvider as React.FC<
-  PropsWithChildren<{ overrides?: Partial<Theme>; ssr: boolean }>
->;
 
 function NextApp({ Component, pageProps }: AppProps) {
   /**
@@ -45,7 +32,7 @@ function NextApp({ Component, pageProps }: AppProps) {
     <NoSSRWrapper>
       <AppBridgeProvider appBridgeInstance={appBridgeInstance}>
         <GraphQLProvider>
-          <ThemeProvider overrides={themeOverrides} ssr>
+          <ThemeProvider >
             <ThemeSynchronizer />
             <RoutePropagator />
             <Component {...pageProps} />
--- src/providers/GraphQLProvider.tsx
+++ src/providers/GraphQLProvider.tsx
@@ -7,6 +7,10 @@ export function GraphQLProvider(props: PropsWithChildren<{}>) {
   const { appBridgeState } = useAppBridge();
   const url = appBridgeState?.saleorApiUrl!;
 
+  if(!url) {
+    return <div>{props.children}</div>
+  }
+
   const client = createClient(url, async () => Promise.resolve({ token: appBridgeState?.token! }));
 
   return <Provider value={client} {...props} />;
