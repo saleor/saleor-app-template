609faeb3fafedc55f8eb6050b03995b831e5565c
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2023-05-09 12:00:46 +0200

    Update template to use new SDK and Macaw. Add example webhook (#168)
    * Update template to use new SDK and Macaw. Add example webhook
    
    * Update URQL
    
    * Update src/pages/api/webhooks/order-created.ts
    
    Co-authored-by: Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
    
    ---------
    
    Co-authored-by: Krzysztof Wolski <krzysztof.k.wolski@gmail.com>

--- src/pages/api/manifest.ts
+++ src/pages/api/manifest.ts
@@ -2,6 +2,7 @@ import { createManifestHandler } from "@saleor/app-sdk/handlers/next";
 import { AppManifest } from "@saleor/app-sdk/types";
 
 import packageJson from "../../../package.json";
+import { orderCreatedWebhook } from "./webhooks/order-created";
 
 export default createManifestHandler({
   async manifestFactory(context) {
@@ -9,30 +10,34 @@ export default createManifestHandler({
       name: packageJson.name,
       tokenTargetUrl: `${context.appBaseUrl}/api/register`,
       appUrl: context.appBaseUrl,
+      /**
+       * Set permissions for app if needed
+       * https://docs.saleor.io/docs/3.x/developer/permissions
+       */
       permissions: [
         /**
-         * Set permissions for app if needed
-         * https://docs.saleor.io/docs/3.x/developer/permissions
+         * Add permission to allow "ORDER_CREATED" webhook registration.
+         *
+         * This can be removed
          */
+        "MANAGE_ORDERS",
       ],
       id: "saleor.app",
       version: packageJson.version,
-      webhooks: [
-        /**
-         * Configure webhooks here. They will be created in Saleor during installation
-         * Read more
-         * https://docs.saleor.io/docs/3.x/developer/api-reference/objects/webhook
-         *
-         * Easiest way to create webhook is to use app-sdk
-         * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-webhook.md
-         */
-      ],
-      extensions: [
-        /**
-         * Optionally, extend Dashboard with custom UIs
-         * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
-         */
-      ],
+      /**
+       * Configure webhooks here. They will be created in Saleor during installation
+       * Read more
+       * https://docs.saleor.io/docs/3.x/developer/api-reference/objects/webhook
+       *
+       * Easiest way to create webhook is to use app-sdk
+       * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-webhook.md
+       */
+      webhooks: [orderCreatedWebhook.getWebhookManifest(context.appBaseUrl)],
+      /**
+       * Optionally, extend Dashboard with custom UIs
+       * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
+       */
+      extensions: [],
     };
 
     return manifest;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,19 @@
 import { NextPage } from "next";
 import { useAppBridge } from "@saleor/app-sdk/app-bridge";
-import { Button } from "@saleor/macaw-ui";
 import dynamic from "next/dynamic";
 import { MouseEventHandler, useEffect, useState } from "react";
-import { Link, TextField } from "@material-ui/core";
+import { Box, Input, Text, Button } from "@saleor/macaw-ui/next";
 
 const ClientContent = dynamic(() => import("../DashboardActions"), {
   ssr: false,
 });
 
 const AddToSaleorForm = () => (
-  <form
-    style={{
-      display: "flex",
-      gap: "2rem",
-    }}
+  <Box as={"form"} display={"flex"} alignItems={"center"} gap={4}
     onSubmit={(event) => {
       event.preventDefault();
 
-      const saleorUrl = new FormData(event.currentTarget).get("saleor-url");
+      const saleorUrl = new FormData(event.currentTarget as HTMLFormElement).get("saleor-url");
       const manifestUrl = new URL("/api/manifest", window.location.origin);
       const redirectUrl = new URL(
         `/dashboard/apps/install?manifestUrl=${manifestUrl}`,
@@ -28,9 +23,9 @@ const AddToSaleorForm = () => (
       window.open(redirectUrl, "_blank");
     }}
   >
-    <TextField type="url" required label="Saleor URL" name="saleor-url" />
+    <Input type="url" required label="Saleor URL" name="saleor-url" />
     <Button type="submit">Add to Saleor</Button>
-  </form>
+  </Box>
 );
 
 /**
@@ -68,62 +63,86 @@ const IndexPage: NextPage = () => {
   };
 
   return (
-    <div>
-      <h1>Welcome to Saleor App Template (Next.js) 🚀</h1>
-      <p>This is a boilerplate you can start with, to create an app connected to Saleor</p>
-      <h2>Resources</h2>
+    <Box padding={8}>
+      <Text variant={"hero"}>Welcome to Saleor App Template (Next.js) 🚀</Text>
+      <Text as={"p"} marginTop={8}>
+        This is a boilerplate you can start with, to create an app connected to Saleor
+      </Text>
+      <Text variant={"heading"} marginTop={8} as={"h2"}>
+        Resources
+      </Text>
       <ul>
         <li>
-          <Link
+          <a
             onClick={handleLinkClick}
             target="_blank"
             rel="noreferrer"
             href="https://github.com/saleor/app-examples"
           >
-            App Examples repository
-          </Link>
+            <Text color={"text3Decorative"}>App Examples repository</Text>
+          </a>
         </li>
         <li>
-          <Link
+          <a
             onClick={handleLinkClick}
             target="_blank"
             rel="noreferrer"
             href="https://github.com/saleor/saleor-app-sdk"
           >
-            Saleor App SDK
-          </Link>
+            <Text color={"text3Decorative"}>Saleor App SDK</Text>
+          </a>
         </li>
         <li>
-          <Link
+          <a
             onClick={handleLinkClick}
             target="_blank"
             href="https://docs.saleor.io/docs/3.x/developer/extending/apps/key-concepts"
             rel="noreferrer"
           >
-            Apps documentation
-          </Link>
+            <Text color={"text3Decorative"}>Apps documentation </Text>
+          </a>
         </li>
         <li>
-          <Link
+          <a
             onClick={handleLinkClick}
             target="_blank"
             href="https://github.com/saleor/saleor-cli"
             rel="noreferrer"
           >
-            Saleor CLI
-          </Link>
+            <Text color={"text3Decorative"}>Saleor CLI</Text>
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            href="https://macaw-ui-next.vercel.app/?path=/docs/getting-started-installation--docs"
+            rel="noreferrer"
+          >
+            <Text color={"text3Decorative"}>Macaw UI - storybook</Text>
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            href="https://github.com/saleor/apps"
+            rel="noreferrer"
+          >
+            <Text color={"text3Decorative"}>Saleor Apps - official apps by Saleor Team</Text>
+          </a>
         </li>
       </ul>
 
       {appBridgeState?.ready && mounted ? (
         <ClientContent />
       ) : (
-        <div>
-          <p>Install this app in your Dashboard and check extra powers!</p>
+        <>
+          <Text marginBottom={4} as={"p"}>Install this app in your Dashboard and check extra powers!</Text>
           {mounted && !global.location.href.includes("localhost") && <AddToSaleorForm />}
-        </div>
+        </>
       )}
-    </div>
+    </Box>
   );
 };
 
