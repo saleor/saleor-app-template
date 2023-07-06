e29450386f95a93d0bbc274e538915808e665a95
Adrian Pilarczyk <admin@peelar.dev>
2023-07-06 11:50:13 +0200

    Documentation improvements (#175)
    * docs: :memo: fix mistakes in README
    
    * docs: :memo: add more resources & instructions in pages/index
    
    * refactor: :recycle: add / to paths and remove installation instructions
    
    * style: :lipstick: add styles to code tag
    
    * fix: :bug: only display install app when not already installed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import { actions, useAppBridge } from "@saleor/app-sdk/app-bridge";
-import { Box, Button, Text } from "@saleor/macaw-ui/next";
+import { Box, Button, Input, Text } from "@saleor/macaw-ui/next";
 import { NextPage } from "next";
 import Link from "next/link";
 import { MouseEventHandler, useEffect, useState } from "react";
+
+const AddToSaleorForm = () => (
+  <Box
+    as={"form"}
+    display={"flex"}
+    alignItems={"center"}
+    gap={4}
+    onSubmit={(event) => {
+      event.preventDefault();
+
+      const saleorUrl = new FormData(event.currentTarget as HTMLFormElement).get("saleor-url");
+      const manifestUrl = new URL("/api/manifest", window.location.origin);
+      const redirectUrl = new URL(
+        `/dashboard/apps/install?manifestUrl=${manifestUrl}`,
+        saleorUrl as string
+      ).href;
+
+      window.open(redirectUrl, "_blank");
+    }}
+  >
+    <Input type="url" required label="Saleor URL" name="saleor-url" />
+    <Button type="submit">Add to Saleor</Button>
+  </Box>
+);
 
 /**
  * This is page publicly accessible from your app.
@@ -36,6 +60,8 @@ const IndexPage: NextPage = () => {
      */
   };
 
+  const isLocalHost = global.location.href.includes("localhost");
+
   return (
     <Box padding={8}>
       <Text variant={"hero"}>Welcome to Saleor App Template (Next.js) 🚀</Text>
@@ -49,30 +75,40 @@ const IndexPage: NextPage = () => {
         </Link>
       )}
 
+      <Text as={"p"} marginTop={8}>
+        Explore the App Template by visiting:
+      </Text>
+      <ul>
+        <li>
+          <code>/src/pages/api/manifest</code> - the{" "}
+          <a
+            href="https://docs.saleor.io/docs/3.x/developer/extending/apps/manifest"
+            target="_blank"
+            rel="noreferrer"
+          >
+            App Manifest
+          </a>
+          .
+        </li>
+        <li>
+          <code>/src/pages/api/webhooks/order-created</code> - an example <code>ORDER_CREATED</code>{" "}
+          webhook handler.
+        </li>
+        <li>
+          <code>/graphql</code> - the pre-defined GraphQL queries.
+        </li>
+        <li>
+          <code>/generated/graphql.ts</code> - the code generated for those queries by{" "}
+          <a target="_blank" rel="noreferrer" href="https://the-guild.dev/graphql/codegen">
+            GraphQL Code Generator
+          </a>
+          .
+        </li>
+      </ul>
       <Text variant={"heading"} marginTop={8} as={"h2"}>
         Resources
       </Text>
       <ul>
-        <li>
-          <a
-            onClick={handleLinkClick}
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/saleor/app-examples"
-          >
-            <Text color={"text3Decorative"}>App Examples repository</Text>
-          </a>
-        </li>
-        <li>
-          <a
-            onClick={handleLinkClick}
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/saleor/saleor-app-sdk"
-          >
-            <Text color={"text3Decorative"}>Saleor App SDK</Text>
-          </a>
-        </li>
         <li>
           <a
             onClick={handleLinkClick}
@@ -87,6 +123,38 @@ const IndexPage: NextPage = () => {
           <a
             onClick={handleLinkClick}
             target="_blank"
+            rel="noreferrer"
+            href="https://docs.saleor.io/docs/3.x/developer/extending/apps/developing-with-tunnels"
+          >
+            <Text color={"text3Decorative"}>Tunneling the app</Text>
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/saleor/app-examples"
+          >
+            <Text color={"text3Decorative"}>App Examples repository</Text>
+          </a>
+        </li>
+
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/saleor/saleor-app-sdk"
+          >
+            <Text color={"text3Decorative"}>Saleor App SDK</Text>
+          </a>
+        </li>
+
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
             href="https://github.com/saleor/saleor-cli"
             rel="noreferrer"
           >
@@ -97,23 +165,42 @@ const IndexPage: NextPage = () => {
           <a
             onClick={handleLinkClick}
             target="_blank"
-            href="https://macaw-ui-next.vercel.app/?path=/docs/getting-started-installation--docs"
+            href="https://github.com/saleor/apps"
             rel="noreferrer"
           >
-            <Text color={"text3Decorative"}>Macaw UI - storybook</Text>
+            <Text color={"text3Decorative"}>Saleor App Store - official apps by Saleor Team</Text>
           </a>
         </li>
         <li>
           <a
             onClick={handleLinkClick}
             target="_blank"
-            href="https://github.com/saleor/apps"
+            href="https://macaw-ui-next.vercel.app/?path=/docs/getting-started-installation--docs"
             rel="noreferrer"
           >
-            <Text color={"text3Decorative"}>Saleor Apps - official apps by Saleor Team</Text>
+            <Text color={"text3Decorative"}>Macaw UI - official Saleor UI library</Text>
+          </a>
+        </li>
+        <li>
+          <a
+            onClick={handleLinkClick}
+            target="_blank"
+            href="https://nextjs.org/docs"
+            rel="noreferrer"
+          >
+            <Text color={"text3Decorative"}>Next.js documentation</Text>
           </a>
         </li>
       </ul>
+
+      {mounted && !isLocalHost && !appBridgeState?.ready && (
+        <>
+          <Text marginBottom={4} as={"p"}>
+            Install this app in your Dashboard and get extra powers!
+          </Text>
+          <AddToSaleorForm />
+        </>
+      )}
     </Box>
   );
 };
