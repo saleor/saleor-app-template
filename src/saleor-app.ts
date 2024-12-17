4faddf3c3b58c8ec599261f6bf95278a54776156
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2024-12-17 14:14:52 +0100

    update sdk and implement vercel kv example (#265)
    * update sdk and implement vercel kv example
    
    * update vercel kv example
    
    * fix switchcase
    
    * update sdk

--- src/saleor-app.ts
+++ src/saleor-app.ts
@@ -1,5 +1,6 @@
 import { SaleorApp } from "@saleor/app-sdk/saleor-app";
 import { APL, FileAPL, UpstashAPL } from "@saleor/app-sdk/APL";
+import { VercelKvApl } from "@saleor/app-sdk/APL/vercel-kv";
 
 /**
  * By default auth data are stored in the `.auth-data.json` (FileAPL).
@@ -10,6 +11,21 @@ import { APL, FileAPL, UpstashAPL } from "@saleor/app-sdk/APL";
  */
 export let apl: APL;
 switch (process.env.APL) {
+  case "vercel-kv":
+    /**
+     * Following envs are required
+     *
+     * KV_URL
+     * KV_REST_API_URL
+     * KV_REST_API_TOKEN
+     * KV_REST_API_READ_ONLY_TOKEN
+     * KV_STORAGE_NAMESPACE
+     *
+     * Additionally, you need
+     * pnpm i @vercel/kv
+     */
+    apl = new VercelKvApl();
+    break;
   case "upstash":
     // Require `UPSTASH_URL` and `UPSTASH_TOKEN` environment variables
     apl = new UpstashAPL();
