35de7c4da44575c5a05c3d1cca8b77b6d44d0293
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2025-01-02 16:09:45 +0100

    Add Web API routes

---
+++ src/app/api-v2/manifest/route.ts
@@ -0,0 +1,58 @@
+import { createManifestHandler } from "@saleor/app-sdk/handlers/fetch-api";
+import { AppManifest } from "@saleor/app-sdk/types";
+import packageJson from "../../../../package.json";
+import { orderCreatedWebhook } from "../../../pages/api/webhooks/order-created";
+
+export const GET = createManifestHandler({
+  manifestFactory({ appBaseUrl, request }) {
+    /**
+     * Allow to overwrite default app base url, to enable Docker support.
+     *
+     * See docs: https://docs.saleor.io/docs/3.x/developer/extending/apps/local-app-development
+     */
+    const iframeBaseUrl = process.env.APP_IFRAME_BASE_URL ?? appBaseUrl;
+    const apiBaseURL = process.env.APP_API_BASE_URL ?? appBaseUrl;
+
+    const manifest: AppManifest = {
+      name: "Saleor App Template",
+      tokenTargetUrl: `${apiBaseURL}/api-v2/register`,
+      appUrl: iframeBaseUrl,
+      /**
+       * Set permissions for app if needed
+       * https://docs.saleor.io/docs/3.x/developer/permissions
+       */
+      permissions: [
+        /**
+         * Add permission to allow "ORDER_CREATED" webhook registration.
+         *
+         * This can be removed
+         */
+        "MANAGE_ORDERS",
+      ],
+      id: "saleor.app",
+      version: packageJson.version,
+      /**
+       * Configure webhooks here. They will be created in Saleor during installation
+       * Read more
+       * https://docs.saleor.io/docs/3.x/developer/api-reference/webhooks/objects/webhook
+       *
+       * Easiest way to create webhook is to use app-sdk
+       * https://github.com/saleor/saleor-app-sdk/blob/main/docs/saleor-webhook.md
+       */
+      webhooks: [orderCreatedWebhook.getWebhookManifest(apiBaseURL)],
+      /**
+       * Optionally, extend Dashboard with custom UIs
+       * https://docs.saleor.io/docs/3.x/developer/extending/apps/extending-dashboard-with-apps
+       */
+      extensions: [],
+      author: "Saleor Commerce",
+      brand: {
+        logo: {
+          default: `${apiBaseURL}/logo.png`,
+        },
+      },
+    };
+
+    return manifest;
+  },
+});
---
+++ src/app/api-v2/register/route.ts
@@ -0,0 +1,26 @@
+import { createAppRegisterHandler } from "@saleor/app-sdk/handlers/fetch-api";
+import { saleorApp } from "../../../saleor-app";
+
+export const runtime = "nodejs"; // FileAPL requires node environment to save files on disk using fs
+
+/**
+ * Required endpoint, called by Saleor to install app.
+ * It will exchange tokens with app, so saleorApp.apl will contain token
+ */
+export const POST = createAppRegisterHandler({
+  apl: saleorApp.apl,
+  allowedSaleorUrls: [
+    /**
+     * You may want your app to work only for certain Saleor instances.
+     *
+     * Your app can work for every Saleor that installs it, but you can
+     * limit it here
+     *
+     * By default, every url is allowed.
+     *
+     * URL should be a full graphQL address, usually starting with https:// and ending with /graphql/
+     *
+     * Alternatively pass a function
+     */
+  ],
+});
